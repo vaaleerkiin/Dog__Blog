1041f35164a6faae3a2029395c19fc8967a31c30
Valera Kamelkov <tahaburrent@gmail.com>
2023-02-09 01:33:25 +0200

    ~

--- src/js/main-gallary.js
+++ src/js/main-gallary.js
@@ -25,25 +25,7 @@ const inlineGallery = lightGallery(lgContainer, {
   slideDelay: 0,
   plugins: [Autoplay, Thumbnail],
   controls: true,
-  dynamicEl: [
-    {
-      src: 'https://zelenvsit.cx.ua/wp-content/uploads/2018/6/sif-2141.jpg',
-      thumb: 'https://zelenvsit.cx.ua/wp-content/uploads/2018/6/sif-2141.jpg',
-      subHtml: `<div class="lightGallery-captions">
-                <h4>Caption 1</h4>
-                <p>Description of the slide 1</p>
-            </div>`,
-    },
-    {
-      src: 'http://i.otzovik.com/objects/b/100000/95554.png',
-      thumb: 'http://i.otzovik.com/objects/b/100000/95554.png',
-      subHtml: `<div class="lightGallery-captions">
-                <h4>Caption 2</h4>
-                <p>Description of the slide 2</p>
-            </div>`,
-    },
-    ...CreateDynemicEl(),
-  ],
+  dynamicEl: [...CreateDynemicEl()],
 });
 console.log(DynemicGallaryEl);
 inlineGallery.openGallery();
@@ -58,17 +40,17 @@ console.log(SortElForDate());
 function CreateDynemicEl() {
   return SortElForDate().map(el => {
     let arr = el;
+
+    arr.subHtml = `<div class="lightGallery-captions">
+                <h4>${arr.header}</h4>
+                <p> ${arr.date}</p>
+            </div>`;
+    arr.src = `https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${arr.src}`;
+    arr.thumb = `https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${arr.thumb}`;
     delete arr.header;
     delete arr.description;
     delete arr.date;
 
-    arr.subHtml = `<div class="lightGallery-captions">
-                <h4>${arr.header}</h4>
-                <p>${arr.description} ${arr.date}</p>
-            </div>`;
-    arr.src = `https://vaaleerkiin.github.io/Dog__Blog/${arr.src}`;
-    arr.thumb = `https://vaaleerkiin.github.io/Dog__Blog/${arr.thumb}`;
     return arr;
   });
 }
-console.log(CreateDynemicEl());
