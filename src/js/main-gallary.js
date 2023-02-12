4e820eee1b796b92e006c18038f268a0779579e9
Valera Kamelkov <tahaburrent@gmail.com>
2023-02-12 23:57:54 +0200

    ~

--- src/js/main-gallary.js
+++ src/js/main-gallary.js
@@ -26,7 +26,7 @@ const inlineGallery = lightGallery(lgContainer, {
   slideDelay: 0,
   plugins: [Autoplay, Thumbnail],
   controls: true,
-  dynamicEl: [...CreateDynemicEl(8)],
+  dynamicEl: CreateDynemicEl(8),
 });
 
 setTimeout(() => {
@@ -34,25 +34,30 @@ setTimeout(() => {
 }, 300);
 function SortElForDate() {
   const arr = DynemicGallaryEl;
-  return arr.sort((a, b) => {
-    return new Date(b.date) - new Date(a.date);
-  });
+  return arr.sort((a, b) => new Date(b.date) - new Date(a.date));
 }
 
 function CreateDynemicEl(quantity) {
-  const el = [];
-  for (let i = 0; i < quantity; i += 1) {
-    const SortEl = SortElForDate()[i];
-    SortEl.subHtml = `<div class="lightGallery-captions">
-                <h4>${SortEl.header}</h4>
-                 <p> ${SortEl.date}</p>
-             </div>`;
-    SortEl.src = `https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${SortEl.src}`;
-    SortEl.thumb = `https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${SortEl.thumb}`;
-    delete SortEl.header;
-    delete SortEl.description;
-    delete SortEl.date;
-    el.push(SortEl);
-  }
-  return el;
+  return SortElForDate()
+    .map(el => {
+      const date = `${el.date}`;
+      const year = date.substr(0, 4);
+      const month = date.substr(4, 2);
+      const day = date.substr(6, 2);
+      const hour = date.substr(8, 2);
+      const minute = date.substr(10, 2);
+      el.date = year + '/' + month + '/' + day + '/' + hour + ':' + minute;
+
+      el.subHtml = `<div class="lightGallery-captions">
+                <h4>${el.header}</h4>
+                <p> ${el.date}</p>
+            </div>`;
+      el.src = `https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${el.src}`;
+      el.thumb = `https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${el.thumb}`;
+      delete el.header;
+      delete el.description;
+      delete el.date;
+      return el;
+    })
+    .slice(0, quantity);
 }
--- src/js/news.js
+++ src/js/news.js
@@ -1,33 +1,39 @@
 import MainNewsEl from './source/MainNewsEl.json';
 function SortElForDate() {
   const arr = MainNewsEl;
-  return arr.sort((a, b) => {
-    return new Date(b.date) - new Date(a.date);
-  });
+  return arr.sort((a, b) => new Date(b.date) - new Date(a.date));
 }
 function CreateNewsEl(quantity) {
-  const el = [];
-  for (let i = 0; i < quantity; i += 1) {
-    const SortEl = SortElForDate()[i];
+  return SortElForDate()
+    .map(el => {
+      const date = `${el.date}`;
+      const year = date.substr(0, 4);
+      const month = date.substr(4, 2);
+      const day = date.substr(6, 2);
+      const hour = date.substr(8, 2);
+      const minute = date.substr(10, 2);
+      el.date = year + '/' + month + '/' + day + '/' + hour + ':' + minute;
 
-    el.push(` <li class="news__item">
+      return ` <li class="news__item">
           <div class="news__inf-wrap">
             <div class="news__img-wrap">
               <img
-                src="https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${SortEl.src}"
+                src="https://raw.githubusercontent.com/vaaleerkiin/Dog__Blog/main/src/${el.src}"
                 alt="Фото до новини"
               />
             </div>
             <div class="news__text-wrap">
               <p>
-                ${SortEl.header} 
+                ${el.header} 
               </p>
-              <p class="news__date">${SortEl.date}</p>
+              <p class="news__date">${el.date}</p>
               <button class="btn news__btn">Читати далі</button>
             </div>
           </div>
-        </li>`);
-  }
-  return el;
+        </li>`;
+    })
+    .slice(0, quantity);
 }
+
+// console.log(CreateNewsEl(4));
 document.querySelector('.news__list').innerHTML = CreateNewsEl(4).join('');
